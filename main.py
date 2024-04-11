9f37cfe07032b0423dc37673f54ffc2595c60f05
ammar <ammaratif559@gmail.com>
2024-04-12 00:14:01 +0530

    re

--- main.py
+++ main.py
@@ -1,73 +1,100 @@
+from requests import get, post
 import json
-from dotenv import load_dotenv
 import os
-import base64
-from requests import post, get
 
-load_dotenv()
+token = os.getenv("ACCESS_TOKEN")
+print(f"{token = }")
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+# def user_autherization():
+#     url = "https://accounts.spotify.com/authorize"
+#     query = {
+#         "client_id": client_id,
+#         "response_type": "code",
+#         "redirect_uri": "http://localhost:8080",
+#         "scope": "user-read-private user-read-email"
+#     }
+#     result = get(url, params=query)
+#     print(result.url)
 
-# print(client_id, client_secret)
-
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
-
-# print(get_token())
+# user_autherization()
 # quit()
 
 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
-def search_for_artist(token, artist_name):
+def search_for_artist(token, artist_name, limit=1):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
-    query = f"q={artist_name}&type=artist&limit=1"
+    query = f"q={artist_name}&type=artist&limit={limit}"
 
     query_url = url + "?" + query
     result = get(query_url, headers=headers)
     json_result = json.loads(result.content)["artists"]["items"]
+    artists = []
+    for item in json_result:
+        artist_name = item["name"]
+        uri = item["uri"]
+        followers = item["followers"]["total"]
+        genres = item["genres"]
+        artists += artist_name
+        
+        print(f"{artist_name = }".ljust(40), end='')
+        print(f"{followers = }".ljust(25), end='')
+        print(f"{uri = }".ljust(55), end='')
+        print(f"{genres = }")
+        
+    # print(json.dumps(json_result, indent=4), sep='\n')
+    
     if len(json_result) == 0:
         print("No such artist exists...")
-        return None
 
-    return json_result[0]
-
-    print(json.dumps(json_result, indent=4))
-
-def search_for_playlist(token, playlist_name):
+def search_for_playlist(token, playlist_name, limit=1):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
-    query = f"q={playlist_name}&type=playlist&limit=10"
+    query = f"q={playlist_name}&type=playlist&limit={limit}"
 
     query_url = url + "?" + query
     result = get(query_url, headers=headers)
     json_result = json.loads(result.content)["playlists"]["items"]
-    print(json.dumps(json_result, indent=4))
+    for item in json_result:
+        name = item["name"]
+        owner = item["owner"]["display_name"]
+        uri = item["uri"]
+        
+        print(f"{owner = }".ljust(45, " "), end='')
+        print(f"playlist {name = }".ljust(60, " "), end='')
+        print(f"{uri = }")
+        
+    # print(json.dumps(json_result, indent=4))
     return json_result
 
-
-
-def get_songs_by_artist(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+def get_songs_by_artist(token, artist_name, limit=5):
+    def get_artist_id(artist_name, limit=1):
+        url = "https://api.spotify.com/v1/search"
+        headers = get_auth_header(token)
+        query = f"q={artist_name}&type=artist&limit={limit}"
+        query_url = url + "?" + query
+        
+        result = get(query_url, headers=headers)
+        if result.status_code != 401:
+            json_result = json.loads(result.content)["artists"]["items"]
+            print(f"{json_result = }")
+            
+            id = json_result[0]["id"]
+            print(f"{artist_name = }, {id = }")
+            return id
+        else:
+            json_result = json.loads(result.content)
+            print(json_result)
+        
+    artist_id = get_artist_id(artist_name) 
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US&limit={limit}"
     headers = get_auth_header(token)
+    
     result = get(url, headers=headers)
     json_result = json.loads(result.content)["tracks"]
+    print(json.dumps(json_result, indent=4))
     return json_result
 
 def get_user_saved_albums(token):
@@ -79,17 +106,20 @@ def get_user_saved_albums(token):
     json_result = json.loads(result.content)
     print(json.dumps(json_result, indent=4))
 
-token = os.getenv("ACCESS_TOKEN")
-result = search_for_artist(token, "eminem")
-print(result["name"])
+
+# result = search_for_artist(token, "eminem", 10)
+
+# result = search_for_playlist(token, "Best songs 2024", 25)
+
+# result = get_songs_by_artist(token, "eminem")
 
 # artist_id = result["id"]
-# songs = get_songs_ny_artist(token, artist_id)
-# for i, song in enumerate(songs):
-#     print(f"{i+1}. {song['name']}")
+songs = get_songs_by_artist(token, artist_id)
+for i, song in enumerate(songs):
+    print(f"{i+1}. {song['name']}")
 
 # result = search_for_playlist(token, "pop")
 # for i, playlist in enumerate(result):
 #     print(f"{i}. {playlist['external_urls'][w'spotify']} \t  total: {playlist['tracks']['total']} \t {playlist['name']}")
 
-get_user_saved_albums(token)
+# get_user_saved_albums(token)
